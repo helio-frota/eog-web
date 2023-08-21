9633e0b8145c6bad51cc69f89eef0330685c7d43
Helio Frota <00hf11@gmail.com>
2023-08-21 12:04:54 -0300

    chore: structure

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration as WebpackConfig } from 'webpack';
+import { Configuration as WebpackDevServerConfig } from 'webpack-dev-server';
+
+type Configuration = WebpackConfig & { devServer?: WebpackDevServerConfig; }
+
+const config: Configuration = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, 
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [ 
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+  ],
+  devtool: 'inline-source-map',
+};
+
+export default config;
