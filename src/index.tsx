886854d51174017e3705a564bbdcb40cee5416ed
Helio Frota <00hf11@gmail.com>
2023-08-23 07:24:49 -0300

    chore: squares ok

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,13 @@ const root = createRoot(document.getElementById('root') as HTMLElement);
 function App() {
   return (
     <>
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
         <h2 style={{color: 'rgb(38 139 210)'}}><Score /></h2>
         <h2 style={{color: 'rgb(38 139 210)'}}><Steps /></h2>
         <h2 style={{color: 'rgb(38 139 210)'}}><Timer /></h2>
-        <hr style={{color: 'rgb(38 139 210)', borderColor : 'rgb(38 139 210)'}}/>
+      </div>
+      <hr style={{color: 'rgb(38 139 210)', borderColor : 'rgb(38 139 210)'}}/>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
         <World />
       </div>
     </>
